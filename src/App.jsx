a8db0010d71c458c7286dd8277c5f8eec4bde8d9
cdcrypto <74511588+cdcrypto@users.noreply.github.com>
2024-04-08 14:58:05 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   // Equivalent to Vue's onMounted
   const fetchWalletInfo = () => {
-    if (window.Telegram.WebApp) {
+    if (window.Telegram && window.Telegram.WebApp) {
       const telegramId = window.Telegram.WebApp.initDataUnsafe.user.id;
   
       fetch('https://77nypb.buildship.run/id', {
